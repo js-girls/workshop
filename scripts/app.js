a4bcff12397ab8bff3115ae45ddf213a274cdf73
Vincenzo Acinapura <vin.aci@gmail.com>
2015-10-25 10:31:34 +0100

    Moves things around a bit

--- scripts/app.js
+++ scripts/app.js
@@ -1,50 +1,50 @@
-var todoItems = [
-  { text: "Buy pasta",   completed: true },
-  { text: "Buy milk",    completed: false },
-  { text: "Disco dance", completed: false }
+var taskItems = [
+  { text: 'Buy coffee',  completed: true  },
+  { text: 'Buy milk',    completed: false },
+  { text: 'Disco dance', completed: false }
 ];
 
-var renderItem = function(item, template) {
-  var todoClass = ['list-group-item'];
+var renderItem = function(item) {
+  var template = document.querySelector('#item-template').innerHTML;
+  var classAttr = ['list-group-item'];
 
-  if (item['completed']) todoClass.push('list-group-item-success');
+  if (item['completed']) classAttr.push('list-group-item-success');
 
   return template
     .replace('_TEXT_', item['text'])
-    .replace('_CLASS_', todoClass.join(' '))
+    .replace('_CLASS_', classAttr.join(' '))
     .replace('_CHECKED_', item['completed'] ? 'checked' : '');
 }
 
 var renderList = function(items) {
   var listElement = document.querySelector('#todo-list');
-  var itemTemplate = document.querySelector('#template-todo-item').innerHTML;
 
   listElement.innerHTML = '';
 
   items.forEach(function(item) {
-    listElement.innerHTML += renderItem(item, itemTemplate);
+    listElement.innerHTML += renderItem(item);
   });
 }
 
 var toggleStatus = function(event) {
   var text = event.target.parentNode.querySelector('span').innerHTML;
 
-  todoItems = todoItems.map(function(item) {
+  taskItems = taskItems.map(function(item) {
     if (item['text'] == text) item['completed'] = !item['completed'];
     return item;
   });
 
-  renderList(todoItems);
+  renderList(taskItems);
 }
 
 var removeItem = function(event) {
   var text = event.target.parentNode.querySelector('span').innerHTML;
 
-  todoItems = todoItems.filter(function(item) {
+  taskItems = taskItems.filter(function(item) {
     if (item['text'] != text) return true;
   });
 
-  renderList(todoItems);
+  renderList(taskItems);
 }
 
 var createNew = function(event) {
@@ -53,10 +53,10 @@ var createNew = function(event) {
   if (event.keyCode != 13) return;
   if (newItemElement.value.trim() == '') return;
 
-  todoItems.push({ text: newItemElement.value, completed: false });
+  taskItems.push({ text: newItemElement.value, completed: false });
   newItemElement.value = '';
 
-  renderList(todoItems);
+  renderList(taskItems);
 }
 
 var showItems = function(what) {
@@ -64,17 +64,17 @@ var showItems = function(what) {
 
   switch(what) {
     case 'completed':
-      itemsToShow = todoItems.filter(function(item) {
+      itemsToShow = taskItems.filter(function(item) {
         return item['completed'];
       });
       break;
     case 'active':
-      itemsToShow = todoItems.filter(function(item) {
+      itemsToShow = taskItems.filter(function(item) {
         return !item['completed'];
       });
       break;
     default:
-      itemsToShow = todoItems;
+      itemsToShow = taskItems;
       break;
   }
 
@@ -82,11 +82,11 @@ var showItems = function(what) {
 }
 
 var clearCompleted = function() {
-  todoItems = todoItems.filter(function(item) {
+  taskItems = taskItems.filter(function(item) {
     return !item['completed'];
   });
 
-  renderList(todoItems);
+  renderList(taskItems);
 }
 
-renderList(todoItems);
+renderList(taskItems);
